363b08a0c22b162cb1d01853226a245c02339a54
Lee.wiseman@bourne-leisure.co.uk <leew21@hotmail.co.uk>
2022-02-23 13:04:18 +0000

    feat: set up react app

---
+++ my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+import HelloWorld from './components/NavMenu'
+
+const App = () => {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
+
